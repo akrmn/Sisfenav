623048d20334a5338ed5031c94aa3cb5fd8f3c8c
akrmn <unacson@gmail.com>
2015-05-14 21:22:08 -0430

    Missing: giving back empty places

--- client.c
+++ client.c
@@ -2,56 +2,134 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
+
+#define DEFPORT 8888
 
 void error(const char *msg)
 {
-    perror(msg);
-    exit(0);
+  perror(msg);
+  exit(0);
+}
+
+void argread(
+  int argc,
+  char const *argv[],
+  int * rowno,
+  int * colno,
+  char ** hostname,
+  int * portno
+){
+  int rflag = 0; // rows
+  int cflag = 0; // cols
+  int hflag = 0; // hostname
+  int pflag = 0; // port
+
+  int index;
+  int c;
+  char *cvalue = NULL;
+  while ((c = getopt (argc, argv, "r:c:h:p:")) != -1)
+    switch (c) {
+      case 'r':
+        rflag = 1;
+        sscanf (optarg,"%d", rowno);
+        break;
+      case 'c':
+        cflag = 1;
+        sscanf (optarg,"%d", colno);
+        break;
+      case 'h':
+        hflag = 1;
+        * hostname = optarg;
+        break;
+      case 'p':
+        pflag = 1;
+        sscanf (optarg,"%d", portno);
+        break;
+      default:
+        fprintf(
+          stderr,
+          "Unknown flag -%c",
+          c
+        );
+        exit(1);
+    }
+
+  if (!hflag){
+    fprintf(stderr, "Must give host name or IP address with -h option\n");
+    exit(1);
+  }
+
+  if (!rflag){
+    fprintf(stderr, "Must give row number with -r option\n");
+    exit(1);
+  }
+
+  if (!cflag){
+    fprintf(stderr, "Must give column number with -c option\n");
+    exit(1);
+  }
+
+  if (!pflag){
+    printf("Using default port, %d\n", DEFPORT);
+    * portno = DEFPORT;
+  }
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+  int rowno, colno;
+  int portno;
+  char * hostname;
 
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
-    return 0;
+  argread(argc, argv, &rowno, &colno, &hostname, &portno);
+
+
+  int sockfd, n;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+
+  char buffer[64];
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+    error("ERROR opening socket");
+  server = gethostbyname(hostname);
+  if (server == NULL) {
+    fprintf(stderr,"ERROR, no such host\n");
+    exit(0);
+  }
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy(
+    (char *)server->h_addr_list[0],
+    (char *)&serv_addr.sin_addr.s_addr,
+    server->h_length
+  );
+  serv_addr.sin_port = htons(portno);
+  if (
+    connect(
+      sockfd,
+      (struct sockaddr *) &serv_addr,
+      sizeof(serv_addr)
+    ) < 0
+  )
+    error("ERROR connecting");
+
+  bzero(buffer,64);
+  sprintf(buffer, "%d:%d;", rowno, colno);
+  n = write(sockfd, buffer, strlen(buffer));
+  if (n < 0)
+    error("ERROR writing to socket");
+  bzero(buffer,64);
+  n = read(sockfd,buffer,63);
+  if (n < 0)
+    error("ERROR reading from socket");
+  printf("%s\n",buffer);
+  close(sockfd);
+  return 0;
 }
--- server.c
+++ server.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <getopt.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,114 +12,175 @@
 #define OCCUPIED 1
 #define AVAILABLE 0
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(1);
-}
+#define MAXROWS 100
+#define DEFROWS 10
+#define MAXCOLS 100
+#define DEFCOLS 4
+
+#define DEFPORT 8888
 
 void argread(
-    int argc,
-    char const *argv[],
-    int &rows,
-    int &cols,
-    int &portno
-)
-{
+  int argc,
+  char const *argv[],
+  int * rows,
+  int * cols,
+  int * portno
+){
+  int rflag = 0; // rows
+  int cflag = 0; // cols
+  int pflag = 0; // port
 
+  // int index;
+  int c;
+  char *cvalue = NULL;
+  while ((c = getopt (argc, argv, "r:c:p:")) != -1)
+    switch (c) {
+      case 'r':
+        rflag = 1;
+        sscanf (optarg,"%d", rows);
+        if (*rows > MAXROWS || rows <= 0){
+          fprintf(
+            stderr,
+            "Invalid number of rows, number must be "
+            "larger than zero and smaller than %d\n",
+            MAXROWS
+          );
+          exit(1);
+        }
+        break;
+      case 'c':
+        cflag = 1;
+        sscanf (optarg,"%d", cols);
+        if (*cols > MAXCOLS || cols <= 0){
+          fprintf(
+            stderr,
+            "Invalid number of columns, number must be "
+            "larger than zero and smaller than %d\n",
+            MAXCOLS
+          );
+          exit(1);
+        }
+        break;
+      case 'p':
+        pflag = 1;
+        sscanf (optarg,"%d", portno);
+        break;
+      default:
+        fprintf(
+          stderr,
+          "Unknown flag -%c",
+          c
+        );
+        exit(1);
+    }
+
+  if (!rflag)
+    * rows = DEFROWS;
+
+  if (!cflag)
+    * cols = DEFCOLS;
+
+  if (!pflag)
+    * portno = DEFPORT;
+}
+
+void error(
+  const char *msg
+){
+  perror(msg);
+  exit(1);
 }
 
 int reserve(
-    int ** places,
-    int rowno,
-    int colno
+  int ** places,
+  int rowno,
+  int colno
 ){
-    if (places[rowno][colno] == AVAILABLE){
-        places[rowno][colno] = OCCUPIED;
-        return 1;
-    } else {
-        return 0;
-    }
+  if (places[rowno][colno] == AVAILABLE){
+    places[rowno][colno] = OCCUPIED;
+    return 1;
+  } else {
+    return 0;
+  }
 }
-
 
 int main(int argc, char const *argv[])
 {
-    int rows, cols, freeplaces;
+  int rows, cols;
+  int portno;
+  argread(argc, argv, &rows, &cols, &portno);
+  int freeplaces = rows * cols;
 
-    int sockfd, newsockfd, portno, n;
-    socklen_t clilen;
-    char buffer[64];
-    struct sockaddr_in serv_addr, cli_addr;
 
-    argread(argc, argv, &rows, &cols, &portno)
+  int sockfd, newsockfd, n;
+  socklen_t clilen;
+  char buffer[64];
+  struct sockaddr_in serv_addr, cli_addr;
 
-    int **places = malloc(rows * sizeof(int *));
-    places[0] = malloc(rows * cols * sizeof(int));
-    for(i = 1; i < rows; i++)
-        places[i] = places[0] + i * cols;
+  int **places = malloc(rows * sizeof(int *));
+  places[0] = malloc(rows * cols * sizeof(int));
+  int i;
+  for(i = 1; i < rows; i++)
+    places[i] = places[0] + i * cols;
 
-    freeplaces = rows * cols;
+  sockfd  = socket(PF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+    error("ERROR opening socket");
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
 
-    sockfd  = socket(PF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
+  if (
+    bind(
+      sockfd,
+      (struct sockaddr *) &serv_addr,
+      sizeof(serv_addr)
+    ) < 0
+  ) error("ERROR on binding");
+
+  listen(sockfd, 5);
+
+  while(1){
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(
+      sockfd,
+      (struct sockaddr *) &cli_addr,
+      &clilen
+    );
+    if (newsockfd < 0)
+      error("ERROR on accept");
+
+    bzero(buffer,64);
+    n = read(newsockfd,buffer,63);
+
+    if (n < 0) error("ERROR reading from socket");
+
+    int rowno, colno;
+    sscanf(
+      buffer,
+      "%d:%d;",
+      &rowno,
+      &colno
+    );
+
+    rowno--;
+    colno--;
 
     if (
-        bind(
-            sockfd,
-            (struct sockaddr *) &serv_addr,
-            sizeof(serv_addr)
-        ) < 0
-    ) error("ERROR on binding");
-
-    listen(sockfd, 5);
-
-    while(1){
-        clilen = sizeof(cli_addr);
-        newsockfd = accept(
-            sockfd, 
-            (struct sockaddr *) &cli_addr, 
-            &clilen
-        );
-        if (newsockfd < 0) 
-            error("ERROR on accept");
-        bzero(buffer,64);
-
-        n = read(newsockfd,buffer,63);
-        if (n < 0) error("ERROR reading from socket");
-
-        int rowno, colno;
-        sscanf(
-            buffer,
-            "%d:%d;"
-            rowno,
-            colno
-        );
-
-        rowno--;
-        colno--;
-
-        if (
-            rowno >= rows ||
-            colno >= cols ||
-            rowno < 0 ||
-            colno < 0
-        ){
-            n = write(newsockfd,"3",1); // non-existing place 
-        } else if (freeplaces == 0){
-            n = write(newsockfd,"2",1); // full train
-
-        } else if (reserve(places, rowno, colno)){
-            freeplaces--;
-            n = write(newsockfd,"0",1); // successfully reserved 
-        } else {
-            n = write(newsockfd,"1",1); // occupied place 
-        }
+      rowno >= rows ||
+      colno >= cols ||
+      rowno < 0 ||
+      colno < 0
+    ){
+      n = write(newsockfd,"3",1); // non-existing place
+    } else if (freeplaces == 0){
+      n = write(newsockfd,"2",1); // full train
+    } else if (reserve(places, rowno, colno)){
+      freeplaces--;
+      n = write(newsockfd,"0",1); // successfully reserved
+    } else {
+      n = write(newsockfd,"1",1); // occupied place
     }
-
+  }
 }
